079a3897a873437d3da0e2bf8c50c80d471f3cd1
Atish Panday <atishpanday@Atishs-MacBook-Air.local>
2024-06-05 02:32:10 -0700

    removed blog stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Blogs from "./blogs/page";
-
 export default function Home() {
-  return (
-    <main className="m-4">
-      <h1>Hello</h1>
-    </main>
-  );
+    return (
+        <main className="m-4">
+            <h1>Hello</h1>
+        </main>
+    );
 }
